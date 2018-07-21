9b34a4d9a9f57e23e7d98b54798f87fc74281086
cz.goroh <cz.goroh@gmail.com>
2018-07-21 19:00:40 +0300

    beta1.1 21.07.18

--- logics/actions/rmanch.php
+++ logics/actions/rmanch.php
@@ -1,6 +1,48 @@
 <?php
 //$post= Secure::PostText($_POST);
-
+$st_id=$id;
+if(isset($_POST['acp'])){
+    $id_rol= Secure::PostText($_POST)['acp'];
+    $statser= Dbq::AtomSel('status', 'rolik', 'id', $id_rol);
+    $statarr= unserialize($statser);
+    $statarr['app'][]=$st_id;
+    if(!empty($statarr['rej'])){
+    $rejkey= array_keys($statarr['rej'], $st_id)[0];
+    unset($statarr['rej'][$rejkey]);
+    }
+    if(!empty($statarr['upl'])){
+    unset($statarr['upl']);}
+    if(!empty($statarr['red'])){
+    unset($statarr['red']);}
+    $statser= serialize($statarr);
+    $acq="UPDATE rolik SET `status`='$statser' WHERE `id`=$id_rol";
+    Dbq::InsDb($acq);
+    $apq="UPDATE bid SET `status`='man_ap' WHERE `rolik_id`=$id_rol AND "
+            . "`status`='cl_app'";
+    Dbq::InsDb($apq);
+    echo 'Одобрен';
+    exit();
+}
+if(isset($_POST['rj'])):
+//    $post= Secure::PostText($_POST);
+    $rj=$post['rj'];
+    $statser= Dbq::AtomSel('status', 'rolik', 'id', $rj);
+    $statarr= unserialize($statser);
+    $statarr['rej'][]=$st_id;
+    if(!empty($statarr['upl'])){
+    unset($statarr['upl']);}
+    if(!empty($statarr['red'])){
+    unset($statarr['red']);}
+    $statser= serialize($statarr);
+    $descser= Dbq::AtomSel('descr', 'rolik', 'id', $rj);
+    $descrar= unserialize($descser);
+    $descrar[$st_id]=$post['des'];
+    $descser= serialize($descrar);
+    $rjq="UPDATE rolik SET `status`='$statser',`descr`='$descser' WHERE `id`=$rj";
+    Dbq::InsDb($rjq);
+    echo 'отправлено';
+    exit();
+endif;
 if(isset($_POST['upd_loy'])){
     if(!empty($post['loypass'])){
         $phash=password_hash($post['loypass'],PASSWORD_BCRYPT);
